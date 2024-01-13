74deaca9ab7ccfa633e32c71869f3c3762dcaaf0
mtone <>
2024-01-13 15:16:23 -0500

    Repro

---
+++ Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace Shared
+{
+    public class Class1
+    {
+        public void Main()
+        {
+            var entity = new Entity("test");
+        }
+    }
+}
